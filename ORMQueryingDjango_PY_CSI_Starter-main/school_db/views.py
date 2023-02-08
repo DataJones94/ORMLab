544a42ac86b511d530689ef8c7dde578be5bd1e6
unknown <k.jones45@yahoo.com>
2023-02-08 14:57:19 -0500

    Finished problem five.

--- ORMQueryingDjango_PY_CSI_Starter-main/school_db/views.py
+++ ORMQueryingDjango_PY_CSI_Starter-main/school_db/views.py
@@ -249,12 +249,9 @@ SELECT COUNT(*) AS `__count`
 # Print the new student's id, full name, year, and gpa to the terminal
 # NOTE every time you execute this function a duplicate student will be created with a different primary key number
 def problem_five(request):
-    student = Student.objects.create(first_name = 'Keyonna', last_name ='Jones')
-    student = Student.objects.create(year = '12')
-    student = Student.objects.create(gpa = '4.0')
-    student = Student.objects.create(id = '8')
-
-    print(f'Id: {student.id} \n Full Name: {student.first_name + student.last_name} \n Year: {student.year} \n GPA: {student.gpa}')
+    new_student = Student.objects.create( first_name = 'Keyonna', last_name ='Jones', year = '12', gpa = '4.0')
+    
+    print(f'Id: {new_student.id} \n Full Name: {new_student.first_name + new_student.last_name} \n Year: {new_student.year} \n GPA: {new_student.gpa}')
 
     return complete(request)
 
